fef4233f61ae949087581faf9a9abbefff1f77e6
Jeffrey Emakpor <jeffreyefemena4@gmail.com>
2019-08-06 18:20:51 +0100

    [update] add data to resolver

--- src/resolver/dripResolver.js
+++ src/resolver/dripResolver.js
@@ -3,8 +3,38 @@ const {MockList} =  require('graphql-yoga')
 
 const Sales = [{
     id: '0',
-    
+    name: 'Jeffrey test',
+    product: Product,
+    order: Order
 }]
+
+
+const Product = [{
+    id: '1',
+    name: "long Sleeve Men",
+    price: 123.0,
+    brand: "Gucci",
+    size: 12,
+    size_type: "M",
+    category: "Clothing"
+}]
+
+
+const Order = [{
+    id: '1',
+    product: Product,
+    date: "12 Mon 2019",
+    status: "sold",
+    quantity: 2
+}]
+
+
+let saleId = Sales.length;
+
 const resolvers = {
-    
+    createSales: (parent , args) => {
+        const sale = {
+            id = `${saleId++}`
+        }
+    }
 }
